48dc2089f7682b61c642236ba03746322d510ded
code-with-zeeshan <zeeshansayfyebusiness@gmail.com>
2024-08-25 06:38:32 +0530

    script update

--- script.js
+++ script.js
@@ -237,3 +237,15 @@ if ('serviceWorker' in navigator) {
   }
 
 });
+document.addEventListener('focusin', (event) => {
+    if (event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA') {
+        document.querySelector('.footer').style.position = 'absolute';
+        document.querySelector('.footer').style.bottom = 'auto';
+    }
+});
+
+document.addEventListener('focusout', () => {
+    document.querySelector('.footer').style.position = 'fixed';
+    document.querySelector('.footer').style.bottom = '0';
+});
+
